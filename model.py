91de08d7147e72664d9054adb209bccefef4bed9
Seungduk Seo <seseo@student.42seoul.kr>
2024-01-11 23:36:45 +0900

    batch_size, callback, print string added

--- model.py
+++ model.py
@@ -1,153 +1,20 @@
 #!/usr/bin/python3
 
 import sys
+import copy
 import shutil
 import time
+
 import pandas as pd
 import numpy as np
-import copy
-from enum import Enum, auto
-from collections.abc import Iterable
-from optimizer import Optimizer, Adam, SGD
 
-from typing import Any, Iterable, Callable
+from typing import Any, Iterable
 from functools import partial
 
-# import pickle
-# from load_csv import load, get_cur_dir
-
-EPS = 1e-7
-
-class LAYER(Enum):
-    INPUT = auto()
-    DENSE = auto()
-    DROPOUT = auto()
-
-
-class Callback:
-    """callbacks"""
-
-    def __init__(self):
-        pass
-
-
-class EarlyStopping(Callback):
-    """EarlyStopping"""
-
-    def __init__(self, monitor = "val_loss"):
-        self.monitor = monitor
-
-class Initializer:
-    """Initalizer"""
-
-    @staticmethod
-    def glorotNormal(shape: tuple):
-        """Glorot / Xavier normal initializer."""
-        stddev = np.sqrt(2.0 / (shape[0] + shape[1]))
-        return np.random.normal(0, stddev, shape)
-
-    @staticmethod
-    def glorotUniform(shape: tuple):
-        """Glorot / Xavier uniform initializer."""
-        limit = np.sqrt(6.0 / (shape[0] + shape[1]))
-        return np.random.uniform(-limit, limit, shape)
-
-    @staticmethod
-    def heNormal(shape: tuple):
-        """He normal initializer."""
-        stddev = np.sqrt(2.0 / shape[0])
-        return np.random.normal(0, stddev, shape)
-
-    @staticmethod
-    def heUniform(shape: tuple):
-        """He uniform initializer."""
-        limit = np.sqrt(6.0 / shape[0])
-        return np.random.uniform(-limit, limit, shape)
-
-    @staticmethod
-    def zeros(shape: tuple):
-        """Zero initializer. For bias."""
-        return np.zeros(shape)
-
-
-class Layers:
-    """
-    layer class
-    
-    """
-
-    layer_to_str = {
-        LAYER.INPUT:"Input",
-        LAYER.DENSE:"Dense",
-        LAYER.DROPOUT:"Dropout",
-    }
-
-    def __init__(
-        self,
-        layer_type: LAYER,
-        layer_dim: int,
-        activation: str,
-        weights_initializer: str | Callable
-    ):
-        """layer init."""
-        self.type: LAYER = layer_type
-        self.layer_dim = layer_dim
-        self.activation = activation
-
-        if layer_type != LAYER.INPUT:
-            if isinstance(weights_initializer, str):
-                match weights_initializer:
-                    case "heNormal":
-                        self.weights_initializer = Initializer.heNormal
-                    case "heUniform":
-                        self.weights_initializer = Initializer.heUniform
-                    case "glorotNormal" | "xavierNormal":
-                        self.weights_initializer = Initializer.glorotNormal
-                    case "glorotUniform" | "xavierUniform":
-                        self.weights_initializer = Initializer.glorotUniform
-                    case "zero":
-                        self.weights_initializer = Initializer.zeros
-                    case _:
-                        raise AssertionError("Invalid initializer.")
-            
-            elif callable(weights_initializer):
-                self.weights_initializer = weights_initializer
-
-            else:
-                raise AssertionError("Invalid initializer.")
-
-    @staticmethod
-    def Input(
-        layer_dim: int
-    ):
-        """Create dense layer."""
-
-        return Layers(LAYER.INPUT, layer_dim, None, None)
-
-    @staticmethod
-    def Dense(
-        layer_dim: int,
-        activation: str | Callable,
-        weights_initializer: str="glorotNormal"
-    ):
-        """Create dense layer."""
-
-        return Layers(LAYER.DENSE, layer_dim, activation, weights_initializer)
-
-    # @staticmethod
-    # def Dropout(
-
-    # ):
-    #     """Create dropout layer."""
-
-
-    @staticmethod
-    def getLayer(layer: LAYER) -> str:
-        return Layers.layer_to_str[layer]
-
-    def getLayer(self) -> str:
-        return self.layer_to_str[self.type]
-
+from optimizer import Optimizer, Adam, SGD
+from initializer import Initializer
+from layers import LAYER, Layers
+from callback import Callback, EarlyStopping, History
 
 
 class Model:
@@ -164,6 +31,9 @@ class Model:
         self.optimizer: Optimizer = None
         self.loss = None
         self.metrics = None
+
+        self.history = None
+        self.stop_training = False
 
     def _assert_compile(self):
         """compile checker."""
@@ -199,7 +69,7 @@ class Model:
             model.add(l)
         return model
 
-    def compile(self, optimizer, loss, metrics):
+    def compile(self, optimizer, loss, metrics=[]):
         """set optimizer, loss, metrics"""
 
         assert self.iscompile is False, "model is already compiled."
@@ -221,33 +91,51 @@ class Model:
                     raise AssertionError("Invalid optimizer.")
         else:
             raise AssertionError("Optimizer should be string or Optimizer class.")
-            
+        
+        self.optimizer.build(self.params)
+
         self.loss = loss
         self.metrics = metrics
         self.iscompile = True
 
     def fit(
         self,
-        x=None,
-        y=None,
-        epochs=1,
-        batch_size=32,
-        callbacks: list[Callback]=None
+        x: np.ndarray=None,
+        y: np.ndarray=None,
+        batch_size: int=32,
+        epochs: int=1,
+        callbacks: list[Callback]=None,
+        validation_split: float=0.0
     ):
         """train data."""
 
         self._assert_compile()
 
-        history = {}
-        history["loss"] = []
-        
-        batch_max = x.shape[0] // batch_size
-        seed = int(time.time() * 1000000)
+        self.stop_training = False
+        self.history = History()
+        self.history.set_model(self)
+
+        callback_tmp = []
+        if callbacks:
+            callback_tmp = callbacks[:]
+        callback_tmp.append(self.history)
+
+        self._callback_on_train(callback_tmp)
+
+        batch_max, batch_remain = divmod(x.shape[0], batch_size)
+        if batch_remain:
+            batch_max += 1
+        # seed = int(time.time() * 1000000)
         seed = 10
-    
+
         for i in range(epochs):
-            start_time = time.time()
-            loss_tmp = []
+            loss_sum = 0
+            loss = 0
+            acc_sum = 0
+            acc = 0
+            train_size = 0
+            max_str_len = 0
+            logs = {}
 
             # for mini-batch
             np.random.seed(seed)
@@ -255,27 +143,82 @@ class Model:
             shuffled_x = x[permutation, :]
             shuffled_y = y[permutation, :]
 
+            epoch_str = f"{i + 1}/{epochs}"
+            print(epoch_str)
+
             for n_batch in range(batch_max):
+
+                batch_start_time = time.time()
                 mini_batch_x = shuffled_x[n_batch * batch_size: (n_batch + 1) * batch_size]
                 mini_batch_y = shuffled_y[n_batch * batch_size: (n_batch + 1) * batch_size]
-                # print(n_batch * batch_size, (n_batch + 1) * batch_size)
 
+                train_size += mini_batch_x.shape[0]
                 AL, caches = self._model_forward(mini_batch_x)
-                loss_tmp.append(self._compute_cost(AL, mini_batch_y))
+                loss_sum += self._compute_cost(AL, mini_batch_y) * mini_batch_x.shape[0]
+                loss = loss_sum / train_size
+
                 self._model_backward_and_update_params(AL, mini_batch_y, caches)
 
+                batch_end_time = time.time()
+                batch_time_micro_s = int((batch_end_time - batch_start_time) * 1000000)
+                batch_time_ms, batch_time_micro_remain = divmod(batch_time_micro_s, 1000)
+                
+                batch_str = f"\r{n_batch + 1}/{batch_max}"
+
+                batch_time_s = 0
+                if batch_time_ms == 0:
+                    batch_str += f" - 0s {batch_time_micro_remain}μs/step"
+                else:
+                    if batch_time_ms > 1000:
+                        batch_time_s, batch_time_ms = divmod(batch_time_ms, 1000)
+                    batch_str += f" - {batch_time_s}s {batch_time_ms}ms/step"
+                
+                batch_str += f" - loss: {loss:.4f}"
+
+                if hasattr(self.metrics, "Accuracy"):
+                    acc_tmp = self._get_accuracy_sum(AL, mini_batch_y)
+                    if acc_tmp is not None:
+                        acc_sum += acc_tmp
+                        acc = acc_sum / train_size
+                        batch_str += f" - accuracy: {acc:.4f}"
+                
+                if max_str_len < len(batch_str):
+                    max_str_len = len(batch_str)
+                else:
+                    batch_str += " " * (max_str_len - len(batch_str))
+                print(batch_str, end="", flush=True)
+
             seed += 1
+            print("")
 
             # for callback in callbacks:
             #     callback
-            
-            history['loss'].append(np.mean(loss_tmp))
 
-            end_time = time.time()
-            print_str = f"{i+1}/{epochs} - {int((end_time - start_time) * 1000)}ms/step"
-            print(print_str)
+            if self.stop_training:
+                break
 
         return history
+
+    def _callback_on_train(self, callback: list[Callback]):
+        for cb in callback:
+            cb.on_train_begin()
+
+    def _get_accuracy_sum(self, y_hat: np.ndarray, y: np.ndarray):
+        """get accuracy"""
+
+        match self.loss:
+            case "binaryCrossentropy":
+                if y_hat.shape[1] != 1:
+                    y_hat = get_one_hot_value(y_hat)
+                    return np.sum(np.all(y_hat == y, axis=1))
+                elif y.ndim == 1 or y.shape[1] == 1:
+                    y_hat = y_hat >= 0.5
+                    return np.sum(y_hat)
+                else:
+                    raise AssertionError("loss error")
+            case _:
+                return None
+
 
     def evaluate(self,
                  x=None,
@@ -290,6 +233,14 @@ class Model:
 
         self._assert_compile()
 
+        predict, _ = self._model_forward(x)
+
+        match self.loss:
+            case "binaryCrossentropy":
+                if predict.shape[1] != 1:
+                    return get_one_hot_value(predict)
+
+        return predict
 
     def summary(self):
         """Show layer structure."""
@@ -509,8 +460,6 @@ class Model:
 
         """
 
-        # m = AL.shape[1]
-
         match self.loss:
             case "binaryCrossentropy":
                 dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
@@ -519,7 +468,6 @@ class Model:
             case _:
                 raise AssertionError("Invalid loss function.")
 
-
         for i, layer in reversed(list(enumerate(self.layer[1:], start=0))):
             cache = caches[i]
             dAL, dW_tmp, db_tmp = self._linear_activation_backward(dAL, cache, layer.activation)
@@ -527,23 +475,9 @@ class Model:
             self.grads["dW" + str(i + 1)] = dW_tmp
             self.grads["db" + str(i + 1)] = db_tmp
 
-            # # self._update_param()
-
-            # # # parameters will updated by the optimizer.
-            # # self.params["W" + str(i + 1)] = self.optimizer.update(self.params["W" + str(i + 1)], dW_tmp)
-            # # self.params["b" + str(i + 1)] = self.optimizer.update(self.params["b" + str(i + 1)], db_tmp)
-            # self.params["W" + str(i + 1)] = self.params["W" + str(i + 1)] - self.optimizer.learning_rate * dW_tmp
-            # self.params["b" + str(i + 1)] = self.params["b" + str(i + 1)] - self.optimizer.learning_rate * db_tmp
-        return self.grads
-
-    def _update_param(self):
-        """
-        Args
-            AL: the output of the last layer
-            Y: the answer vector of input data
-        """
-
         self.optimizer.update(self.params, self.grads)
+
+        return self.grads
 
 
 def relu(x: np.ndarray, max_val=None):
@@ -564,16 +498,14 @@ def softmax(z: np.ndarray) -> np.ndarray:
     exp_z = np.exp(z - np.max(z, axis=-1, keepdims=True))
     return exp_z / np.sum(exp_z, axis=-1, keepdims=True)
 
+def get_one_hot_value(x: np.ndarray) -> np.ndarray:
+    """one hot value for categorical. Max value -> 1, and the others -> 0."""
 
-# def get_one_hot_value(x: np.ndarray) -> np.ndarray:
-#     """one hot value for categorical. Max value -> 1, and the others -> 0."""
-
-#     max_idx = np.argmax(x, axis=1)
-#     res = np.zeros_like(x)
-#     for i in range(len(max_idx)):
-#         res[i, max_idx[i]] = 1
-#     return res
-
+    max_idx = np.argmax(x, axis=1)
+    res = np.zeros_like(x)
+    for i in range(len(max_idx)):
+        res[i, max_idx[i]] = 1
+    return res
 
 def one_hot_encoding(x: pd.Series) -> np.ndarray:
     """convert categorical variable to binary vector."""
@@ -581,199 +513,6 @@ def one_hot_encoding(x: pd.Series) -> np.ndarray:
     unique_val = x.unique()
     unique_val.sort()
     return (x.values.reshape(-1, 1) == unique_val).astype(int)
-
-
-
-
-
-# def GD_optimizer(w: np.ndarray, b: np.ndarray,
-#                  X: np.ndarray, Y: np.ndarray,
-#                  epoch: int = 1,
-#                  lr: float = 0.001,
-#                  print_cost: int = 0) -> tuple[np.ndarray, np.ndarray, list[float]]:
-#     """
-#     Args
-#         w: weights (n_category, n_feature)
-#         b: bias (n_category,)
-#         X: train data X (n_data, n_feature)
-#         Y: train data Y (n_data, n_category)
-#         epoch: number of iterations
-#         lr: learning rate
-#         print_cost: printing the loss every given steps
-    
-#     Return
-#         tuple(w, b, costs)
-#         costs: cost list of every 100 steps
-#     """
-
-#     return BGD_optimizer(w, b, X, Y, epoch=epoch, batch=X.shape[0], lr=lr, print_cost=print_cost)
-
-
-# def SGD_optimizer(w: np.ndarray, b: np.ndarray,
-#                   X: np.ndarray, Y: np.ndarray,
-#                   epoch: int = 1,
-#                   lr: float = 0.001,
-#                   print_cost: int = 0) -> tuple[np.ndarray, np.ndarray, list[float]]:
-#     """
-#     Args
-#         w: weights (n_category, n_feature)
-#         b: bias (n_category,)
-#         X: train data X (n_data, n_feature)
-#         Y: train data Y (n_data, n_category)
-#         epoch: number of iterations
-#         lr: learning rate
-#         print_cost: printing the loss every given steps
-    
-#     Return
-#         tuple(w, b, costs)
-#         costs: cost list of every 100 steps
-#     """
-
-#     return BGD_optimizer(w, b, X, Y, epoch=epoch, batch=1, lr=lr, print_cost=print_cost)
-
-
-# def BGD_optimizer(w: np.ndarray, b: np.ndarray,
-#                   X: np.ndarray, Y: np.ndarray,
-#                   epoch: int = 1,
-#                   batch: int = 32,
-#                   lr: float = 0.001,
-#                   print_cost: int = 0) -> tuple[np.ndarray, np.ndarray, list[float]]:
-#     """
-#     Args
-#         w: weights (n_category, n_feature)
-#         b: bias (n_category,)
-#         X: train data X (n_data, n_feature)
-#         Y: train data Y (n_data, n_category)
-#         epoch: number of iterations
-#         batch: data size for updating w, b
-#         lr: learning rate
-#         print_cost: printing the loss every given steps
-    
-#     Return
-#         tuple(w, b, costs)
-#         costs: cost list of every 100 steps
-#     """
-
-#     w = copy.deepcopy(w)
-#     b = copy.deepcopy(b)
-
-#     costs = []
-
-#     lim = len(X) // batch + (1 if len(X) % batch else 0)
-
-#     for i in range(epoch):
-
-#         # batch loop
-#         for j in range(lim):
-        
-#             # calculate dw, db, cost
-#             dw, db, cost = propagate(w, b, X[batch * j: batch * (j + 1)], Y[batch * j: batch * (j + 1)])
-
-#             # update w, b
-#             w = w - lr * dw
-#             b = b - lr * db
-
-#         if i % 100 == 0:
-#             costs.append(cost)
-
-#         if print_cost and i % print_cost == 0:
-#             print (f"The cost of iteration {i}: {cost}")
-
-#         # # check converged
-#         # if (np.isclose(dw, np.zeros_like(dw), rtol=1e-6).all() or
-#         #     np.isclose(db, np.zeros_like(db), rtol=1e-6).all()):
-#         #     break
-
-#     costs.append(cost)
-
-#     return w, b, costs
-
-
-# def predict(w: np.ndarray, b: np.ndarray, X: np.ndarray) -> np.ndarray:
-#     """
-#     Args
-#         w: weights (n_category, n_feature)
-#         b: bias (n_category,)
-#         X: train data X (n_data, n_feature)
-
-#     Return
-#         Y_pred: predict value (n_data, n_category)
-#     """
-
-#     m = X.shape[0]
-
-#     A = sigmoid(X @ w.T + b)
-
-#     if w.shape[0] == 1:
-#         Y_pred = (A > 0.5).astype(int)
-#         # for i in range(A.shape[1]):
-#         #     Y_pred[i, 0] = 1 if A[i, 0] > 0.5 else 0
-#     else:
-#         Y_pred = get_one_hot_value(A)
-
-#     return Y_pred.astype(int)
-
-
-# def fit(x_train: np.ndarray, y_train: np.ndarray,
-#           w_init: np.ndarray = None, b_init: np.ndarray = None,
-#           epoch: int = 1,
-#           batch: int = 32,
-#           lr: int = 0.001,
-#           print_cost: int = 100,
-#           optimizer: str = "BGD") -> tuple[np.ndarray, np.ndarray, list[float]]:
-#     """
-#     Args
-#         x_train: train data for input (n_data, n_feature)
-#         y_train: train data for output (n_data, n_category)
-#         w_init: init w (n_category, n_feature)
-#         b_init: inti bias (n_category,)
-#         epoch: number of iterations
-#         batch: data size for updating w, b
-#         lr: learning rate
-#         print_cost: printing the cost every n steps. 0 or None for not printing.
-    
-#     Return
-#         tuple(w, b, costs)
-#         costs: cost list of every 100 steps
-#     """
-
-#     assert len(y_train.shape) in (1, 2), "invalid y value."
-#     assert optimizer in ["BGD", "SGD", "GD"], "invalid optimizer."
-
-#     if len(y_train.shape) == 1:
-#         y_train = y_train.reshape(-1, 1)
-
-#     n_feature = x_train.shape[1]
-#     n_category = y_train.shape[1]
-
-#     # w shape is (n_category, n_feature), and b shape is (n_category,)
-#     if w_init is None:
-#         w_init = np.zeros((n_category, n_feature))
-#     if b_init is None:
-#         b_init = np.zeros(n_category)
-
-#     match optimizer:
-#         case "BGD":
-#             w, b, costs = BGD_optimizer(w_init, b_init, x_train, y_train, epoch, batch, lr, print_cost)
-#         case "SGD":
-#             w, b, costs = SGD_optimizer(w_init, b_init, x_train, y_train, epoch, lr, print_cost)
-#         case "GD":
-#             w, b, costs = GD_optimizer(w_init, b_init, x_train, y_train, epoch, lr, print_cost)
-
-#     return w, b, costs
-
-
-
-# def evaluate(w: np.ndarray, b: np.ndarray, x_test: np.ndarray, y_test: np.ndarray) -> float:
-#     """return accuracy of prediction."""
-    
-#     y_pred = predict(w, b, x_test)
-    
-#     assert y_pred.shape == y_test.shape, "invalid input. different shape"
-
-#     wrong = np.sum(np.abs(y_pred - y_test) / 2)
-    
-#     return (1 - wrong / len(y_test)) * 100
 
 
 
@@ -807,4 +546,4 @@ if __name__ == "__main__":
 
     mlp.compile(optimizer="adam", loss="binaryCrossentropy", metrics=['Accuracy'])
 
-    history = mlp.fit(x_train, y_train, 1000, batch_size=32)
+    history = mlp.fit(x_train, y_train, batch_size=32, epochs=1000)
