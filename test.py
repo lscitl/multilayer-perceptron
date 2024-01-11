5d320d3e4b8f600b9d67d3b2c964a44bce70b009
Seungduk Seo <seseo@student.42seoul.kr>
2024-01-11 23:37:16 +0900

    testing

--- test.py
+++ test.py
@@ -3,10 +3,16 @@ from functools import partial
 
 class tmp:
 
+    i = 0
+
     def __init__(self, a, b) -> None:
         print("class tmp init called.")
+        tmp.i += 1
         self.a = a
         self.b = b
+
+    def __repr__(self):
+        return "tmp"
 
 
 class tmp2(tmp):
@@ -15,17 +21,27 @@ class tmp2(tmp):
         print("class tmp2 init called.")
         super().__init__(a, b)
 
+    def __repr__(self):
+        return "tmp2"
 class tmp3(tmp2):
 
     def __init__(self, a=3, b=3):
         print("class tmp3 init called.")
         super().__init__(a, b)
 
+    def __repr__(self):
+        return "tmp3"
+
 
 if __name__ == "__main__":
 
     t3 = tmp3()
     t2 = tmp2()
+
+    lst = [t2, t3]
+
+    lst2 = lst[:]
+    lst2.append(tmp(1, 2))
 
     print(t3.a)
     print(t2.a)
@@ -36,3 +52,5 @@ if __name__ == "__main__":
     print(isinstance(t2, tmp2))
     print(isinstance(t2, tmp3))
 
+    print(tmp.i)
+    print()
