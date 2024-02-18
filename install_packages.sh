7da782b52bf936eef796d7d7a9b6e4efdfa676de
Seungduk Seo <seseo@student.42seoul.kr>
2024-02-18 21:47:04 +0900

    install messages added.

--- install_packages.sh
+++ install_packages.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-brew install python@3.10
+if ! command -v python3.10 &> /dev/null; then
+    echo "python 3.10 is not found. Install python 3.10"
+    brew install python@3.10
+fi
 
+# setup venv
+echo "setup virtual environment."
 if [ ! -e ./bin/activate ]; then
     if [ ! -e ~/goinfre/.brew/bin/python3.10 ]; then
         python3.10 -m venv ./
@@ -12,6 +17,9 @@ fi
 
 source ./bin/activate
 
-pip install --upgrade pip
+echo "upgrade pip and install packages..."
+pip -q install --upgrade pip
+pip -q install numpy pandas matplotlib black seaborn tensorflow build
 
-pip install numpy pandas matplotlib black seaborn tensorflow build
+echo "environment setting is finished!"
+echo "run 'source ./bin/activate' to activate virtual environment."
