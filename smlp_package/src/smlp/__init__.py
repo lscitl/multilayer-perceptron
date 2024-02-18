29babb7b1c0a46b66f394af532384ff3c81edd4e
Seungduk Seo <seseo@student.42seoul.kr>
2024-02-18 22:52:43 +0900

    import modules

--- smlp_package/src/smlp/__init__.py
+++ smlp_package/src/smlp/__init__.py
@@ -1,2 +1,6 @@
 from smlp.models import Model, Sequential
-from smlp.layers import Layers
+import smlp.callbacks as callbacks
+import smlp.optimizers as optimizers
+import smlp.layers as layers
+import smlp.initializers as initializers
+import smlp.activations as activations
